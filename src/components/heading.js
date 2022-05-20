fcada2f4ce1688fa52a734164fe6927b008c1eb3
nkg-laufire <nkg@laufire.in>
2022-05-20 18:58:16 +0530

    Temp.

--- src/components/heading.js
+++ src/components/heading.js
@@ -4,6 +4,7 @@ import {
 	Box, Breadcrumbs, Link,
 	Typography, Button, ButtonGroup,
 	FormControl, Select, InputLabel,
+	Grid,
 } from '@mui/material';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import HelpIcon from '@mui/icons-material/Help';
@@ -24,48 +25,63 @@ const Heading = ({ state: { view }, actions: { setView }}) => {
 				justifyContent="space-between"
 				alignItems="center"
 			>
-				<Breadcrumbs separator="›" aria-label="breadcrumb">
-					<Link color="inherit" href="/">
-						Home
-					</Link>
-					<Typography color="textPrimary">
-						Gene Centric: PCSK9
-					</Typography>
-				</Breadcrumbs>
-				<Box display="flex" sx={ { p: 1 } }>
-					<FormControl variant="outlined" sx={ { mr: 1, p: 0 } }>
-						<InputLabel
-							htmlFor="outlined-age-native-simple"
-						>
-							<ArrowDownwardIcon/> Download
-						</InputLabel>
-						<Select
-							native={ true }
-							label="Age"
-							displayEmpty={ true }
-							inputProps={ {
-								'name': 'age',
-								'id': 'outlined-age-native-simple',
-								'aria-label': 'Without label',
-							} }
-						>
-							<option aria-label="None" value=""/>
-							<option value={ 10 }>JSON</option>
-							<option value={ 20 }>CSV</option>
-						</Select>
-					</FormControl>
-					<Button
-						variant="outlined"
-						onClick={ setView }
-						sx={ { mr: 1, p: 0.5 } }
-					>
-						{ view === 'plot'
-							? <BarChartIcon/>
-							: <BackupTableIcon/>} { view } </Button>
-					<ButtonGroup color="primary">
-						<Button><HelpIcon/></Button>
-					</ButtonGroup>
-				</Box>
+				<Grid container={ true } spacing={ 2 }>
+					<Grid item={ true } xs={ 4 }>
+						<Breadcrumbs separator="›" aria-label="breadcrumb">
+							<Link color="inherit" href="/">
+								Home
+							</Link>
+							<Typography color="textPrimary">
+								Gene Centric: PCSK9
+							</Typography>
+						</Breadcrumbs>
+					</Grid>
+					<Grid item={ true } xs={ 5 }/>
+					<Grid item={ true } xs={ 3 }>
+						<Grid container={ true }>
+							<Grid item={ true } xs={ 4 }>
+								<FormControl variant="outlined" sx={ { mr: 2 } }>
+									<InputLabel
+										htmlFor="outlined-age-native-simple"
+									>
+										<ArrowDownwardIcon/> Download
+									</InputLabel>
+									<Select
+										native={ true }
+										label="Age"
+										displayEmpty={ true }
+										inputProps={ {
+											'name': 'age',
+											'id': 'outlined-age-native-simple',
+											'aria-label': 'Without label',
+										} }
+									>
+										<option aria-label="None" value=""/>
+										<option value={ 10 }>JSON</option>
+										<option value={ 20 }>CSV</option>
+									</Select>
+								</FormControl>
+							</Grid>
+							<Grid item={ true } xs={ 4 }>
+								<Button
+									variant="outlined"
+									onClick={ setView }
+									sx={ { mr: 1, p: 0.5 } }
+								>
+									{ view === 'plot'
+										? <BarChartIcon/>
+										: <BackupTableIcon/>}
+									{ view }
+								</Button>
+							</Grid>
+							<Grid item={ true } xs={ 4 }>
+								<ButtonGroup color="primary">
+									<Button><HelpIcon/></Button>
+								</ButtonGroup>
+							</Grid>
+						</Grid>
+					</Grid>
+				</Grid>
 			</Box>
 		</ThemeProvider>
 
