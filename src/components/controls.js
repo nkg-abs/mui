22fbf297848c1509294de055ced06d5f045a42ca
nkg-laufire <nkg@laufire.in>
2022-05-23 15:47:21 +0530

    Improved styling.

--- src/components/controls.js
+++ src/components/controls.js
@@ -3,12 +3,21 @@ import Sliders from './sliders';
 import Paper from '@mui/material/Paper';
 import Catergories from './categories';
 import Navigation from './navigation';
+import { Grid, Box } from '@mui/material';
 
 const Controls = () =>
-	<Paper display="flex" justifyContent="space-around">
-		<Catergories/>
-		<Sliders/>
-		<Navigation/>
+	<Paper spacing={ 2 } sx={ { p: 5 } }>
+		<Box display="flex" justifyContent="space-between">
+			<Grid container={ true }>
+				<Grid item={ true } xs={ 4 }>
+					<Catergories/>
+				</Grid>
+				<Grid item={ true } xs={ 4 }>
+					<Sliders/>
+				</Grid>
+			</Grid>
+			<Navigation/>
+		</Box>
 	</Paper>
 	;
 
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { Button } from '@material-ui/core';
 import ReplayIcon from '@mui/icons-material/Replay';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { Box } from '@mui/material';
+import Stack from '@mui/material/Stack';
 
 const Navigation = () =>
-	<>
-		<Button variant="outlined"><ReplayIcon/> Reload</Button>
-		<Button variant="outlined"><MoreHorizIcon/></Button>
-		<Button variant="outlined">G</Button>
-	</>;
+	<Box>
+		<Stack spacing={ 2 } sx={ { '& > button': { p: 1.5 }} } direction="row">
+			<Button
+				variant="outlined"
+			><ReplayIcon/> Reload</Button>
+			<Button variant="outlined"><MoreHorizIcon/></Button>
+		</Stack>
+	</Box>;
 
 export default Navigation;
